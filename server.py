ba5a99deb4b562616b28cbfd9b2fe168db63ee9f
sjperez1 <sarahpz479@gmail.com>
2022-06-10 19:34:15 -0600

    initialize repo

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'secrets,secrets'
+
+@app.route("/")
+def display_form():
+    return render_template("index.html")
+
+@app.route("/process", methods = ["POST"])
+def process_form():
+    session["name"] = request.form["name"]
+    session["location"] = request.form["location"]
+    session["language"] = request.form["language"]
+    session["comment"] = request.form["comment"]
+    return redirect("/result")
+
+@app.route("/result")
+def result():
+    return render_template("result.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
